51b8a6df1743dc0e6018544943aee087ee96c3b8
Adrien Demarez <adrien.demarez@free.fr>
2026-03-17 00:48:40 +1100

    Improvements on the web UI
    Loading a much more lightweight geojson.gz (+ a "minmax.json" used for the
    colormap). Add a Github icon

--- fradiodb.py
+++ fradiodb.py
@@ -178,11 +178,17 @@ def query(dbfilename, itemid, itemtype="site"):
     conn = sqlite3.connect(dbfilename)
     conn.row_factory = dict_factory
     if itemtype=="site":
-        return json.dumps(query_supports(conn, itemid))
+        return json.dumps(query_site(conn, itemid))
     elif itemtype=='support':
         return json.dumps(query_antennas(conn, itemid))
     elif itemtype=='antenna':
         return json.dumps(query_transmitters(conn, itemid))
+
+def query_site(conn, site_id):
+    cur  = conn.cursor()
+    res = cur.execute("""select dms, lon, lat, address, postcode, inseecode from sites where sites.id=?""", (site_id,)).fetchall()[0]
+    res['supports'] = query_supports(conn, site_id)
+    return res
 
 def query_supports(conn, site_id):
     cur  = conn.cursor()
@@ -205,47 +211,45 @@ def query_antennas(conn, support_id):
 
 def query_transmitters(conn, antenna_id):
     cur  = conn.cursor()
-    res_transmitters = cur.execute("""select station_name, operator, system, date_switchedon from transmitters
+    res_transmitters = cur.execute("""select station_name, operator, system, date(date_switchedon,'unixepoch') date_on, bandstr band_kHz
+                                         from transmitters
                                          inner join id_systems on system_id=id_systems.id
                                          inner join stations on station_id=stations.id
                                          inner join id_operators on operator_id=id_operators.id
+                                         inner join v_bandgroups on bandgroup_id=v_bandgroups.id
                                          where antenna_id=?""", (antenna_id,)).fetchall()
     return res_transmitters 
 
 class fradiodb_web_handler(BaseHTTPRequestHandler):
+    def staticfile(self, mime):
+        enc = self.headers.get("Accept-Encoding", "")
+        encoding = None
+        if "gzip" in enc and exists(getcwd()+self.path+'.gz'):
+            self.path += ".gz"
+            encoding = "gzip"
+        try:
+            with open(getcwd()+self.path, "rb") as f:
+                content = f.read()
+            self.send_response(200)
+            self.send_header("Content-Type", mime)
+            if encoding:
+                self.send_header("Content-Encoding", encoding)
+            self.end_headers()
+            self.wfile.write(content)
+        except FileNotFoundError:
+            self.send_error(404)
+        return
+
     def do_GET(self):
         if self.path=="/": self.path="/index.html"
         if self.path == "/index.html":
-            try:
-                with open(getcwd()+self.path, "rb") as f:
-                    content = f.read()
-                self.send_response(200)
-                self.send_header("Content-Type", "text/html")
-                self.end_headers()
-                self.wfile.write(content)
-            except FileNotFoundError:
-                self.send_error(404)
-            return
+            return self.staticfile("text/html")
+        if self.path == "/minmax.json":
+            return self.staticfile("application/json")
+        if self.path == "/anfr.json":
+            return self.staticfile("application/geo+json")
         if self.path == "/anfr.fgb":
-            encoding = None
-            enc = self.headers.get("Accept-Encoding", "")
-            if "gzip" in enc and exists("anfr.fgb.gz"):
-                self.path += ".gz"
-                encoding = "gzip"
-                print("gzip")
-            try:
-                with open(getcwd()+self.path, "rb") as f:
-                    content = f.read()
-                self.send_response(200)
-                self.send_header("Content-Type", "application/x-flatgeobuf")
-                if encoding:
-                    self.send_header("Content-Encoding", encoding)
-                self.end_headers()
-                self.wfile.write(content)
-            except FileNotFoundError:
-                self.send_error(404)
-            return
-
+            return self.staticfile("application/x-flatgeobuf")
 
         # Dynamic route /site/<site_id>
         match = re.match(r"^/site/([^/]+)$", self.path)
@@ -260,6 +264,34 @@ class fradiodb_web_handler(BaseHTTPRequestHandler):
 
         # Otherwise 404
         self.send_error(404)
+
+def gen_minmax_json(dbfilename="anfr2026.gpkg", jsonfile="minmax.json"):
+    conn = sqlite3.connect(dbfilename)
+    conn.row_factory = dict_factory
+    query = """select min(support_count) sup_c_min, max(support_count) sup_c_max, min(h) h_min, max(h) h_max,
+               min(operator_count) op_c_min, max(operator_count) op_c_max, min(sta_count) sta_c_min, max(sta_count) sta_c_max,
+               min(ant_count) ant_c_min, max(ant_count) ant_c_max
+                   from (select count(distinct supports.id) support_count, max(sup_height) h, count(distinct operator) operator_count,
+                    count(distinct station_id) sta_count,count(distinct antennas.id) ant_count
+                    from sites
+                    inner join antennas on antennas.sup_id=supports.id
+                    inner join supports on sites.id=supports.site_id
+                    inner join transmitters on transmitters.antenna_id=antennas.id
+                    inner join id_systems on transmitters.system_id=id_systems.id
+                    inner join stations on stations.id=transmitters.station_id
+                    inner join id_operators on id_operators.id=stations.operator_id
+                    group by sites.id)"""
+    cur = conn.cursor()
+    res = cur.execute(query).fetchall()[0]
+    metrics = {
+        "h":  [res['h_min'], res['h_max']],
+        "support_count": [res['sup_c_min'], res['sup_c_max']],
+        "operator_count": [res['op_c_min'], res['op_c_max']],
+        "sta_count": [res['sta_c_min'], res['sta_c_max']],
+        "ant_count": [res['ant_c_min'], res['ant_c_max']],
+    }
+    with open(jsonfile, "w") as f:
+        json.dump(metrics, f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- gen_fgb.sh
+++ gen_fgb.sh
@@ -1,19 +1,56 @@
-rm anfr.fgb
-ogr2ogr -f FlatGeobuf anfr.fgb anfr2026.gpkg -sql "select sites.id, CAST(sites.id AS INTEGER)+0 as site_id, sites.dms, lon, lat, address, postcode, inseecode, geom,
-    count(distinct operator) operator_count, cast(group_concat(distinct operator) as text) operator_list,
-    count(distinct supports.id) support_count, max(sup_height) h_max,
-    count(distinct station_id) sta_count,
-    count(distinct antennas.id) ant_count,
-    count(distinct system) tech_count, cast(group_concat(distinct system) as text) tech_list,
-    count(distinct transmitters.id) tx_count,
-    count(distinct bandstr) band_count,
+#rm anfr.fgb
+#rm anfr.json anfr2.json
+#FlatGeobuf
+# ogr2ogr -f GeoJSON anfr_all.json anfr2026.gpkg -sql "select sites.id, CAST(sites.id AS INTEGER)+0 as site_id, sites.dms, lon, lat, address, postcode, inseecode, geom,
+#     count(distinct operator) operator_count, cast(group_concat(distinct operator) as text) operator_list,
+#     count(distinct supports.id) support_count, max(sup_height) h_max,
+#     count(distinct station_id) sta_count,
+#     count(distinct antennas.id) ant_count,
+#     count(distinct system) tech_count, cast(group_concat(distinct system) as text) tech_list,
+#     count(distinct transmitters.id) tx_count,
+#     count(distinct bandstr) band_count,
+#     sites.tech_bitmask1, sites.tech_bitmask2
+#     from sites
+#     inner join antennas on antennas.sup_id=supports.id
+#     inner join supports on sites.id=supports.site_id
+#     inner join transmitters on transmitters.antenna_id=antennas.id
+#     inner join id_systems on transmitters.system_id=id_systems.id
+#     inner join v_bandgroups on transmitters.bandgroup_id=v_bandgroups.id
+#     inner join stations on stations.id=transmitters.station_id
+#     inner join id_operators on id_operators.id=stations.operator_id
+#     group by sites.id"
+
+#ogr2ogr -f GeoJSON anfr2.json anfr2026.gpkg -sql "select sites.id site_id, geom from sites"
+
+rm anfr.json anfr.json.gz
+ogr2ogr -f GeoJSON anfr.json anfr2026.gpkg -sql "select sites.id, CAST(sites.id AS INTEGER)+0 as site_id, geom,
+    count(distinct supports.id) support_count, max(sup_height) h,
+    count(distinct operator) operator_count,
+    cast(group_concat(distinct operator) as text) operator_list,
+    count(distinct station_id) sta_count,count(distinct antennas.id) ant_count,
     sites.tech_bitmask1, sites.tech_bitmask2
     from sites
     inner join antennas on antennas.sup_id=supports.id
     inner join supports on sites.id=supports.site_id
     inner join transmitters on transmitters.antenna_id=antennas.id
     inner join id_systems on transmitters.system_id=id_systems.id
-    inner join v_bandgroups on transmitters.bandgroup_id=v_bandgroups.id
     inner join stations on stations.id=transmitters.station_id
     inner join id_operators on id_operators.id=stations.operator_id
     group by sites.id"
+gzip anfr.json
+
+sqlite3 -json anfr2026.gpkg "select min(support_count) sup_c_min, max(support_count) sup_c_max, min(h) h_min, max(h) h_max,
+min(operator_count) op_c_min, max(operator_count) op_c_max, min(sta_count) sta_c_min, max(sta_count) sta_c_max,
+min(ant_count) ant_c_min, max(ant_count) ant_c_max
+from (select count(distinct supports.id) support_count, max(sup_height) h, count(distinct operator) operator_count,
+    count(distinct station_id) sta_count,count(distinct antennas.id) ant_count
+    from sites
+    inner join antennas on antennas.sup_id=supports.id
+    inner join supports on sites.id=supports.site_id
+    inner join transmitters on transmitters.antenna_id=antennas.id
+    inner join id_systems on transmitters.system_id=id_systems.id
+    inner join stations on stations.id=transmitters.station_id
+    inner join id_operators on id_operators.id=stations.operator_id
+    group by sites.id)"
+
+
